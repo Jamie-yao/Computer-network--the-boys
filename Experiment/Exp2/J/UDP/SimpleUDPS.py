deae58b61151279082190ba26ba31fcb3360f999
rjszz <1225475362@qq.com>
2020-03-25 23:55:45 +0800

    要提交的变更： 	修改：     "Discussion/Dis1/\350\256\241\347\247\2211702\347\254\254\345\233\233\347\273\204.ppt" 	新文件：   Experiment/Exp2/J/UDP/SimpleUDPC.py 	新文件：   Experiment/Exp2/J/UDP/SimpleUDPS.py 	新文件：   Experiment/Exp2/README.md 	新文件：   "Experiment/Exp2/\345\256\236\351\252\214\344\272\214 .docx"

---
+++ Experiment/Exp2/J/UDP/SimpleUDPS.py
@@ -0,0 +1,36 @@
+# -*- encoding=utf-8 -*-
+
+
+import socket,threading
+
+def server():
+    # 1. 创建套接字
+    s = socket.socket()
+    # 2. 绑定
+    HOST = '0.0.0.0'
+    PORT = 6666
+    s.bind((HOST, PORT))
+    # 3. 监听
+    s.listen(10)
+    # 4. 处理
+    while True:
+        c, addr = s.accept()
+        t1=threading.Thread(target=Server_thread,args=(c, addr))
+        t1.start()
+
+    s.close()
+    pass
+    
+def Server_thread(c, addr):
+    print('Connect client: ', addr)
+    while True:
+        msg = c.recv(1024)
+        if not msg:
+            print('Disconnect client: ', addr)
+            break;
+        print('From client: ', addr,msg.decode())
+        c.sendall(msg)
+    pass    
+
+if __name__ == '__main__':
+    server()
