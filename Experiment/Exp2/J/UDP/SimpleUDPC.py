a8f5412ef6bcd8c030a5145a359580562c136688
rjszz <1225475362@qq.com>
2020-03-26 10:50:06 +0800

    Changes to be committed: 	new file:   Experiment/Exp2/J/TCP/SimpleSocketC.py 	copied:     Experiment/Exp2/J/UDP/SimpleUDPS.py -> Experiment/Exp2/J/TCP/SimpleSocketS.py 	modified:   Experiment/Exp2/J/UDP/SimpleUDPC.py 	modified:   Experiment/Exp2/J/UDP/SimpleUDPS.py 	new file:   "Experiment/Exp2/~$\345\256\236\351\252\214\344\272\214 .docx"

--- Experiment/Exp2/J/UDP/SimpleUDPC.py
+++ Experiment/Exp2/J/UDP/SimpleUDPC.py
@@ -8,17 +8,14 @@ def client():
     HOST=input('请输入IP地址：')
     PORT=input('请输入port端口号：')
     # 1. 创建套接字
-    s = socket.socket()
-    # 2. 连接
-#     HOST = '127.0.0.1'
-#     PORT = 6666
-    s.connect((HOST, int(PORT)))
-    # 3. 处理信息
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
     while True:
-        str=input("请输入:")
-        s.sendall(str.encode())
-        msg = s.recv(1024)
-        print('From server: %s' % msg.decode())
+	    msg=input("请输入:")
+	    if not msg:continue
+	    s.sendto(msg.encode(),(HOST,int(PORT)))
+	    back_msg,addr=s.recvfrom(1024)
+	    print(addr,back_msg.decode())
 
 
 if __name__ == '__main__':
--- Experiment/Exp2/J/UDP/SimpleUDPS.py
+++ Experiment/Exp2/J/UDP/SimpleUDPS.py
@@ -5,32 +5,19 @@ import socket,threading
 
 def server():
     # 1. 创建套接字
-    s = socket.socket()
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     # 2. 绑定
     HOST = '0.0.0.0'
     PORT = 6666
     s.bind((HOST, PORT))
-    # 3. 监听
-    s.listen(10)
-    # 4. 处理
-    while True:
-        c, addr = s.accept()
-        t1=threading.Thread(target=Server_thread,args=(c, addr))
-        t1.start()
 
+    while True:
+	    msg,addr=s.recvfrom(1024)
+	    print(addr,msg.decode())
+	    s.sendto(msg,addr)
     s.close()
     pass
-    
-def Server_thread(c, addr):
-    print('Connect client: ', addr)
-    while True:
-        msg = c.recv(1024)
-        if not msg:
-            print('Disconnect client: ', addr)
-            break;
-        print('From client: ', addr,msg.decode())
-        c.sendall(msg)
-    pass    
+
 
 if __name__ == '__main__':
     server()
